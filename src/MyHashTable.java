8c3ab8150541f1362e4833552e98504e4c41d4b5
ADMIN <221322@astanait.edu.kz>
2023-05-13 22:51:30 +0600

    adding MyHashTable constructor

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -3,18 +3,28 @@ public class MyHashTable<K, V> {
         private K key;
         private V value;
         private HashNode<K, V> next;
-        public HashNode(K key, V value){
-            this.key= key;
-            this.value= value;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
+
         public String toString() {
             return "{" + key + " " + value + "}";
         }
     }
-        private HashNode<K, V>[] chainArray;
-        private int M=11;
-        private int size;
 
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
 
-
+    public MyHashTable() {
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+        size = 0;
+    }
 }
